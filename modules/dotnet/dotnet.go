fbea1a562383817552ff6c590547c34d2ff00613
Philipp Heuer <git@philippheuer.me>
2024-11-21 23:13:39 +0100

    feat: initial support for sln and slnx files

--- modules/dotnet/dotnet.go
+++ modules/dotnet/dotnet.go
@@ -17,6 +17,51 @@ func (a Analyzer) GetName() string {
 func (a Analyzer) Scan(ctx analyzerapi.AnalyzerContext) []*analyzerapi.ProjectModule {
 	var result []*analyzerapi.ProjectModule
 
+	// find solution files
+	for _, file := range ctx.FilesByExtension["sln"] {
+		filename := filepath.Base(file)
+		if strings.HasSuffix(filename, ".sln") {
+			module := analyzerapi.ProjectModule{
+				RootDirectory:     ctx.ProjectDir,
+				Directory:         filepath.Dir(file),
+				Name:              filepath.Base(filepath.Dir(file)),
+				Slug:              slug.Make(filepath.Base(filepath.Dir(file))),
+				Discovery:         []analyzerapi.ProjectModuleDiscovery{{File: file}},
+				Type:              analyzerapi.ModuleTypeBuildSystem,
+				BuildSystem:       analyzerapi.BuildSystemDotNet,
+				BuildSystemSyntax: analyzerapi.BuildSystemSyntaxDotNetSLN,
+				Language:          nil,
+				Dependencies:      nil,
+				Submodules:        nil,
+				Files:             ctx.Files,
+				FilesByExtension:  ctx.FilesByExtension,
+			}
+			analyzerapi.AddModuleToResult(&result, &module)
+		}
+	}
+	for _, file := range ctx.FilesByExtension["slnx"] {
+		filename := filepath.Base(file)
+		if strings.HasSuffix(filename, ".slnx") {
+			module := analyzerapi.ProjectModule{
+				RootDirectory:     ctx.ProjectDir,
+				Directory:         filepath.Dir(file),
+				Name:              filepath.Base(filepath.Dir(file)),
+				Slug:              slug.Make(filepath.Base(filepath.Dir(file))),
+				Discovery:         []analyzerapi.ProjectModuleDiscovery{{File: file}},
+				Type:              analyzerapi.ModuleTypeBuildSystem,
+				BuildSystem:       analyzerapi.BuildSystemDotNet,
+				BuildSystemSyntax: analyzerapi.BuildSystemSyntaxDotNetSLNX,
+				Language:          nil,
+				Dependencies:      nil,
+				Submodules:        nil,
+				Files:             ctx.Files,
+				FilesByExtension:  ctx.FilesByExtension,
+			}
+			analyzerapi.AddModuleToResult(&result, &module)
+		}
+	}
+
+	// find project files
 	for _, file := range ctx.FilesByExtension["csproj"] {
 		filename := filepath.Base(file)
 		if strings.HasSuffix(filename, ".csproj") {
@@ -28,7 +73,7 @@ func (a Analyzer) Scan(ctx analyzerapi.AnalyzerContext) []*analyzerapi.ProjectMo
 				Discovery:         []analyzerapi.ProjectModuleDiscovery{{File: file}},
 				Type:              analyzerapi.ModuleTypeBuildSystem,
 				BuildSystem:       analyzerapi.BuildSystemDotNet,
-				BuildSystemSyntax: analyzerapi.BuildSystemSyntaxDefault,
+				BuildSystemSyntax: analyzerapi.BuildSystemSyntaxDotNetCSProj,
 				Language:          nil,
 				Dependencies:      nil,
 				Submodules:        nil,
--- modules/dotnet/dotnet_test.go
+++ modules/dotnet/dotnet_test.go
@@ -17,8 +17,32 @@ func TestAnalyzer_AnalyzeVisualStudioSolution(t *testing.T) {
 	assert.Len(t, result, 1)
 	assert.Equal(t, "dotnet", result[0].Name)
 	assert.Equal(t, analyzerapi.ModuleTypeBuildSystem, result[0].Type)
-	assert.Equal(t, "dotnet", string(result[0].BuildSystem))
-	assert.Equal(t, "default", string(result[0].BuildSystemSyntax))
+	assert.Equal(t, analyzerapi.BuildSystemDotNet, result[0].BuildSystem)
+	assert.Equal(t, analyzerapi.BuildSystemSyntaxDotNetCSProj, result[0].BuildSystemSyntax)
+
+	// print result
+	for i, item := range result {
+		t.Logf("result[%d]: %+v", i, *item)
+	}
+}
+
+func TestAnalyzer_AnalyzeVisualStudioSolutionNested(t *testing.T) {
+	ctx := analyzerapi.GetAnalyzerContext(analyzerapi.GetTestDataDir(t, "dotnet-nested"))
+
+	analyzer := Analyzer{}
+	result := analyzer.Scan(ctx)
+
+	// module
+	assert.Len(t, result, 1)
+	assert.Equal(t, "dotnet-nested", result[0].Name)
+	assert.Equal(t, analyzerapi.ModuleTypeBuildSystem, result[0].Type)
+	assert.Equal(t, analyzerapi.BuildSystemDotNet, result[0].BuildSystem)
+	assert.Equal(t, analyzerapi.BuildSystemSyntaxDotNetSLN, result[0].BuildSystemSyntax)
+	assert.Len(t, result[0].Submodules, 1)
+	assert.Equal(t, "dotnet-nested-hello-world", result[0].Submodules[0].Name)
+	assert.Equal(t, analyzerapi.ModuleTypeBuildSystem, result[0].Submodules[0].Type)
+	assert.Equal(t, analyzerapi.BuildSystemDotNet, result[0].Submodules[0].BuildSystem)
+	assert.Equal(t, analyzerapi.BuildSystemSyntaxDotNetCSProj, result[0].Submodules[0].BuildSystemSyntax)
 
 	// print result
 	for i, item := range result {
