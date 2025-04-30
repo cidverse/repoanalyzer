f3aaf3eec7f3344410a80ceefd2664eb7ccabd22
Philipp Heuer <git@philippheuer.de>
2025-04-30 21:23:45 +0200

    feat: support yaml extension for ansible playbook files

--- modules/ansible/ansible.go
+++ modules/ansible/ansible.go
@@ -40,6 +40,29 @@ func (a Analyzer) Scan(ctx analyzerapi.AnalyzerContext) []*analyzerapi.ProjectMo
 			analyzerapi.AddModuleToResult(&result, &module)
 		}
 	}
+	for _, file := range ctx.FilesByExtension["yaml"] {
+		filename := filepath.Base(file)
+
+		if filename == "playbook.yaml" || strings.HasSuffix(filename, "-playbook.yaml") {
+			module := analyzerapi.ProjectModule{
+				ID:                analyzerapi.GetSlugFromPath(ctx.ProjectDir, file, a.GetName()),
+				RootDirectory:     ctx.ProjectDir,
+				Directory:         filepath.Dir(file),
+				Name:              filepath.Base(filepath.Dir(file)),
+				Slug:              slug.Make(filepath.Base(filepath.Dir(file))),
+				Discovery:         []analyzerapi.ProjectModuleDiscovery{{File: file}},
+				Type:              analyzerapi.ModuleTypeBuildSystem,
+				BuildSystem:       analyzerapi.BuildSystemAnsible,
+				BuildSystemSyntax: analyzerapi.BuildSystemSyntaxDefault,
+				Language:          nil,
+				Dependencies:      nil,
+				Submodules:        nil,
+				Files:             ctx.Files,
+				FilesByExtension:  ctx.FilesByExtension,
+			}
+			analyzerapi.AddModuleToResult(&result, &module)
+		}
+	}
 
 	return result
 }
